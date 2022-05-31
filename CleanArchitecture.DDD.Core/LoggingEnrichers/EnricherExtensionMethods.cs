0b019547b2764796178d54fcf2f70a9c392a176d
Mustafizur Rohman <mustafizur.rohman@weischer.net>
2022-05-31 17:03:41 +0200

    Code formatting

--- CleanArchitecture.DDD.Core/LoggingEnrichers/EnricherExtensionMethods.cs
+++ CleanArchitecture.DDD.Core/LoggingEnrichers/EnricherExtensionMethods.cs
@@ -7,7 +7,9 @@ public static class EnricherExtensionMethods
 {
     public static LoggerConfiguration WithReleaseNumber(this LoggerEnrichmentConfiguration enrich)
     {
-        return enrich is null ? throw new ArgumentNullException(nameof(enrich)) : enrich.With<ReleaseNumberEnricher>();
+        return enrich is null 
+            ? throw new ArgumentNullException(nameof(enrich)) 
+            : enrich.With<ReleaseNumberEnricher>();
     }
 
     public static LoggerConfiguration WithUsername(this LoggerEnrichmentConfiguration enrich)
