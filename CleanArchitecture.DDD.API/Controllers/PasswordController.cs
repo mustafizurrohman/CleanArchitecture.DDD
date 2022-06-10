3c127c65f38600d785fa1cda28a3dd6af24515a3
Mustafizur Rohman <mustafizur.rohman@weischer.net>
2022-06-10 16:00:08 +0200

    Hash password

---
+++ CleanArchitecture.DDD.API/Controllers/PasswordController.cs
@@ -0,0 +1,30 @@
+﻿namespace CleanArchitecture.DDD.API.Controllers;
+
+public class PasswordController : BaseAPIController
+{
+    public PasswordController(IAppServices appServices) : base(appServices)
+    {
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="password"></param>
+    /// <returns></returns>
+    [ApiExplorerSettings(IgnoreApi = false)]
+    [HttpGet("hash", Name = "Hash Password")]
+    [SwaggerOperation(
+        Summary = "Gets doc from a fake external data service",
+        Description = "No or default authentication required",
+        OperationId = "Hash Password",
+        Tags = new[] { "Password" }
+    )]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> HashPassword(string password)
+    {
+        var hashPasswordQuery = new HashPasswordQuery(password);
+        var result = await Mediator.Send(hashPasswordQuery);
+
+        return Ok(result);
+    }
+}
---
+++ CleanArchitecture.DDD.Application/MediatR/Handlers/HashPasswordQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+
+namespace CleanArchitecture.DDD.Application.MediatR.Handlers;
+
+public class HashPasswordQueryHandler : BaseHandler, IRequestHandler<HashPasswordQuery, string>
+{
+    private int NumberOfRounds { get; }
+
+    public HashPasswordQueryHandler(IAppServices appServices)
+        : base(appServices)
+    {
+        // Can be made configurable
+        // Recommendation: Double every 2 years
+        // 100K is recommened for now!
+        NumberOfRounds = 10000;
+    }
+
+    public Task<string> Handle(HashPasswordQuery request, CancellationToken cancellationToken)
+    {
+        var salt = RandomNumberGenerator.GetBytes(32);
+        
+        var hashedPassword = Rfc2898DeriveBytes.Pbkdf2(
+            request.Password,
+            salt,
+            NumberOfRounds,
+            HashAlgorithmName.SHA256,
+            32);
+
+        var computedPassword = Convert.ToBase64String(hashedPassword)
+                       + "."
+                       + Convert.ToBase64String(salt)
+                       + "."
+                       + NumberOfRounds;
+
+        return Task.FromResult(computedPassword);
+
+    }
+}
