a7368dbe0936a120371b284e8a8a681d9c0fbd40
Mustafizur Rohman <mustafizur.rohman@weischer.net>
2022-06-14 16:53:42 +0200

    Improved ValidatorNotFoundException

--- CleanArchitecture.DDD.Application/Exceptions/ValidatorNotFoundException.cs
+++ CleanArchitecture.DDD.Application/Exceptions/ValidatorNotFoundException.cs
@@ -1,13 +1,17 @@
-﻿namespace CleanArchitecture.DDD.Application.Exceptions;
+﻿using CleanArchitecture.DDD.Domain.ValueObjects;
+
+namespace CleanArchitecture.DDD.Application.Exceptions;
 
 internal class ValidatorNotFoundException : ApplicationException
 {
-    private Type TypeName { get; } 
+    private string? TypeName { get; } 
+    private string ClassName { get; }
 
     public ValidatorNotFoundException(Type typ)
     {
-        TypeName = typ;
+        TypeName = typ.FullName;
+        ClassName = $"AbstractValidator<{typ.FullName}>";
     }
 
-    public override string Message => $"Validator for type {nameof(TypeName)} not found.";
+    public override string Message => $"Validator for type {TypeName} not found. Please create a class which inherits from {ClassName}";
 }
