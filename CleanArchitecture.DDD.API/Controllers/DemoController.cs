787e711db3464f50d08854eea2f778f6b6d05659
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2022-06-15 21:06:34 +0200

    Added Validation controller

--- CleanArchitecture.DDD.API/Controllers/DemoController.cs
+++ CleanArchitecture.DDD.API/Controllers/DemoController.cs
@@ -27,38 +27,7 @@ public class DemoController : BaseAPIController
         return Ok(name1 == name2);
     }
 
-    [ApiExplorerSettings(IgnoreApi = false)]
-    [HttpPost("ValueObject/validation", Name = "valueObjectValidation")]
-    [SwaggerOperation(
-        Summary = "Demo of validation of Value Object",
-        Description = "No or default authentication required",
-        OperationId = "Validate Value Object",
-        Tags = new[] { "Demo" }
-    )]
-    public IActionResult TestNameValueObject(string name)
-    {
-        var createdName = NameValueObject.Create(name);
 
-        if (createdName.Error is not null)
-            return BadRequest(createdName.Error.Message);
-        
-        return Ok(createdName.Value);
-    }
-
-    [ApiExplorerSettings(IgnoreApi = false)]
-    [HttpPost("ValueObect/validation/fluentValidationPipeline")]
-    [SwaggerOperation(
-        Summary = "Demo of input validation using FluentValidation",
-        Description = "No or default authentication required",
-        OperationId = "Input Validation",
-        Tags = new[] { "Demo" }
-    )]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public IActionResult CreateName([FromBody] Name name)
-    {
-        return Ok();
-    }
 
     [ApiExplorerSettings(IgnoreApi = false)]
     [HttpPost("exception", Name = "loggingException")]
---
+++ CleanArchitecture.DDD.API/Controllers/ValidationController.cs
@@ -0,0 +1,41 @@
+﻿namespace CleanArchitecture.DDD.API.Controllers;
+
+public class ValidationController : BaseAPIController
+{
+    public ValidationController(IAppServices appServices) : base(appServices)
+    {
+    }
+
+    [ApiExplorerSettings(IgnoreApi = false)]
+    [HttpPost("ValueObject/validation", Name = "valueObjectValidation")]
+    [SwaggerOperation(
+        Summary = "Demo of validation of Value Object",
+        Description = "No or default authentication required",
+        OperationId = "Validate Value Object",
+        Tags = new[] { "Validation" }
+    )]
+    public IActionResult TestNameValueObject(string name)
+    {
+        var createdName = NameValueObject.Create(name);
+
+        if (createdName.Error is not null)
+            return BadRequest(createdName.Error.Message);
+
+        return Ok(createdName.Value);
+    }
+
+    [ApiExplorerSettings(IgnoreApi = false)]
+    [HttpPost("ValueObect/validation/fluentValidationPipeline")]
+    [SwaggerOperation(
+        Summary = "Demo of input validation using FluentValidation",
+        Description = "No or default authentication required",
+        OperationId = "Input Validation",
+        Tags = new[] { "Validation" }
+    )]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult CreateName([FromBody] Name name)
+    {
+        return Ok();
+    }
+}
