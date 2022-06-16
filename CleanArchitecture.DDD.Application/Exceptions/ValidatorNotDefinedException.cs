347a6f7cb45411534b1fb55fe8f22af628bf2217
Mustafizur Rohman <mustafizur.rohman@weischer.net>
2022-06-16 16:59:23 +0200

    Further improved exception message

--- CleanArchitecture.DDD.Application/Exceptions/ValidatorNotDefinedException.cs
+++ CleanArchitecture.DDD.Application/Exceptions/ValidatorNotDefinedException.cs
@@ -1,17 +1,31 @@
-﻿namespace CleanArchitecture.DDD.Application.Exceptions;
+﻿using System.Reflection;
+
+namespace CleanArchitecture.DDD.Application.Exceptions;
 
 internal class ValidatorNotDefinedException : ApplicationException
 {
     private string? TypeName { get; } 
     private string ValidatorClassName { get; }
+    private string AssemblyName { get; }
 
     public ValidatorNotDefinedException(Type typ)
     {
+        
         TypeName = typ.GenericTypeArguments.FirstOrDefault()?.Name ?? typ.Name;
         ValidatorClassName = $"FluentValidation.AbstractValidator<{TypeName}>";
+        
+        AssemblyName = typ.GenericTypeArguments.Any()
+            ? GetAssemblyName(typ.GenericTypeArguments.First())
+            : GetAssemblyName(typ);
     }
 
-    public override string Message => $"Validator for type {TypeName} not defined. " +
-                                      $"Please define a class which has {ValidatorClassName} as base class in the same assembly where {TypeName} is defined " +
-                                      $"or provide the validator explicitely as an argument of the extension method.";
+    private string GetAssemblyName(Type type)
+    {
+        return Assembly.GetAssembly(type)?.GetName().Name ?? string.Empty;
+    }
+
+    public override string Message => $"Validator for type {TypeName} not defined. " + Environment.NewLine +
+                                      $"Please define a class which has {ValidatorClassName} as base class " + 
+                                      $"in the Assembly '{AssemblyName}' where {TypeName} is defined  " + Environment.NewLine +
+                                      "Alternatively provide the validator explicitely as an argument of the extension method.";
 }
