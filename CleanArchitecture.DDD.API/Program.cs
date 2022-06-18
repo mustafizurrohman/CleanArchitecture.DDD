502afffb5b6dc1a46c29888ea3ca4d9b1055251a
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2022-06-18 12:22:23 +0200

    Added startup message

--- CleanArchitecture.DDD.API/Program.cs
+++ CleanArchitecture.DDD.API/Program.cs
@@ -1,3 +1,6 @@
+Console.WriteLine($"{DateTime.Now:dd.MM.yyyy HH:mm:ss}- Starting API ...");
+Console.WriteLine();
+
 // Add services to the container
 var builder = WebApplication.CreateBuilder(args);
 builder = builder.ConfigureApplication();    
