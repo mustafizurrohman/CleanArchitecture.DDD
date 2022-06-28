281e22ff568f611631bb11ecb3dc1d584b86901f
Mustafizur Rohman <mustafizur.rohman@weischer.net>
2022-06-28 10:00:28 +0200

    Resharper

--- CleanArchitecture.DDD.Infrastructure/Persistence/Entities/BaseEntity.cs
+++ CleanArchitecture.DDD.Infrastructure/Persistence/Entities/BaseEntity.cs
@@ -1,7 +1,9 @@
-﻿namespace CleanArchitecture.DDD.Infrastructure.Persistence.Entities;
+﻿using JetBrains.Annotations;
+
+namespace CleanArchitecture.DDD.Infrastructure.Persistence.Entities;
 
 public abstract class BaseEntity
 {
-    public DateTime? CreatedOn { get; set; }
-    public DateTime? UpdatedOn { get; set; }
+    public DateTime? CreatedOn { [UsedImplicitly] get; set; }
+    public DateTime? UpdatedOn { [UsedImplicitly] get; set; }
 }
