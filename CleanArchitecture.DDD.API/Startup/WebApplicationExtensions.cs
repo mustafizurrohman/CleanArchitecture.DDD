b4034bfba3f202a69b9531eb389bd18d487c6375
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2022-07-23 10:48:42 +0200

    Disabled exception logging middleware

--- CleanArchitecture.DDD.API/Startup/WebApplicationExtensions.cs
+++ CleanArchitecture.DDD.API/Startup/WebApplicationExtensions.cs
@@ -34,7 +34,7 @@ public static class WebApplicationExtensions
         });
 
         app.UseProblemDetails();
-        app.UseExceptionLoggingMiddleware();
+        // app.UseExceptionLoggingMiddleware();
 
         app.MigrateDatabase();
 
