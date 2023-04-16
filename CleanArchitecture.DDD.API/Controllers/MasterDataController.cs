5d3a3a5d9deabe6c42940be6852a17076178d938
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2023-04-16 12:48:30 +0200

    Demo: Query on JSON Column

--- CleanArchitecture.DDD.API/Controllers/MasterDataController.cs
+++ CleanArchitecture.DDD.API/Controllers/MasterDataController.cs
@@ -40,5 +40,30 @@ public class MasterDataController : BaseAPIController
         return Ok(response);
     }
 
-    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="includeSoftDeleted"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    [HttpGet("patient")]
+    [SwaggerOperation(
+        Summary = "Retrieves all patients from database ",
+        Description = DefaultDescription,
+        OperationId = "Get All Patients",
+        Tags = new[] { "MasterData" }
+    )]
+    [SwaggerResponse(StatusCodes.Status200OK, "Doctor was retrieved", typeof(IEnumerable<Patient>))]
+    public async Task<IActionResult> Patients(CancellationToken cancellationToken, bool includeSoftDeleted = false)
+    {
+        var patients = await DbContext.Patients
+            .Where(p => p.MasterData.DateOfBirth > DateTime.Now.AddYears(-5))
+            .Select(p => new { p.Fullname, p.MasterData.DateOfBirth})
+            .ToListAsync(cancellationToken);
+
+        return Ok(patients);
+
+    }
+
+
 }
