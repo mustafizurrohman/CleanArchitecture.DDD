062bc5c34da58250d7ab51d5acb9c444cb25eee0
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2023-05-14 10:50:29 +0200

    Minor changes

--- CleanArchitecture.DDD.Application/Services/PasswordComplexityRoundService.cs
+++ CleanArchitecture.DDD.Application/Services/PasswordComplexityRoundService.cs
@@ -3,18 +3,19 @@
 public class PasswordComplexityRoundService
     : IPasswordComplexityRoundService
 {
-    // This can be constant as it was implemented in 2023
+    // This can be constant as it was implemented in 2023 or adjusted along with BaseYearRounds
     private static int BaseYear => 2023;
     
+    // This can be rechecked and both BaseYear and BaseYearRounds can be adjusted at the same time
     private static int BaseYearRounds => 100_000;
 
     public int GetPasswordRounds()
     {
-        var numberOfYearsFromBaseYear = DateTime.Now.Year - BaseYear;
+        double numberOfYearsFromBaseYear = DateTime.Now.Year - BaseYear;
 
         var factor = numberOfYearsFromBaseYear == 0
             ? 1
-            : numberOfYearsFromBaseYear / 2;
+            : (int)Math.Ceiling(numberOfYearsFromBaseYear / 2);
 
         return BaseYearRounds * factor;
     }
